f3550610c106db1c11078974fce79af72ee27e63
httpsquel <bezerra.raquel@outlook.com>
2023-09-10 14:31:49 -0300

    Enviando o repositório local para o remoto

---
+++ src/desafio/ParametrosInvalidosException.java
@@ -0,0 +1,12 @@
+package desafio;
+
+class ParametrosInvalidosException extends Exception {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
